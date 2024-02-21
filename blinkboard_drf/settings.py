58418eb21deff31188900e0ea4215f0436323d48
annieross10 <annieross10@outlookcom>
2024-02-21 12:02:57 +1000

    update rest framework settitng

--- blinkboard_drf/settings.py
+++ blinkboard_drf/settings.py
@@ -14,9 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication'
-        if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
